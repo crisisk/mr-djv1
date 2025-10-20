c41f609a70a31213c629e87beae59cea97776b55
Chris Klokkers <130704324+crisisk@users.noreply.github.com>
2025-10-20 23:36:14 +0200

    chore: remove unused components and dependencies

--- mr-dj-eds-components/src/components/src/components/App.jsx
+++
@@ -1,3 +0,0 @@
-// src/components/App.jsx
-  import React from 'react';
-  import { Header, Footer } from './components'; // Only import what's needed
--- mr-dj-eds-components/src/components/src/pages/index.jsx
+++
@@ -1,5 +0,0 @@
-// src/pages/index.jsx or relevant page file
-import MusicShowcase from '../components/MusicShowcase';
-
-// Add to the existing page content
-<MusicShowcase />
